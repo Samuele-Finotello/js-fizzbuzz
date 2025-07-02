fe2b59d00488aaa56200a5e9658e6262df1fd093
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-07-02 16:56:30 +0200

    [add] condition Buzz

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 for (i = 1; i <= 100; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
-    console.log('FizzBuzz')
+    console.log('FizzBuzz');
   }
   else
     if (i % 3 === 0) {
-      console.log('Fizz')
+      console.log('Fizz');
     }
     else
-      console.log(i)
+      if (i % 5 === 0) {
+        console.log('Buzz');
+      }
+      else
+        console.log(i);
 }
