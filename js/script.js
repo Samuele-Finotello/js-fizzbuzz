737c373946e85017e0789bb3b37082393b495caa
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-07-02 17:15:17 +0200

    [add] comments

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
+console.log('Ecco la lista di numeri da 1 a 100:');
+
+//ciclo for per comparare ogni i
 for (i = 1; i <= 100; i++) {
+  //col primo if controllo se i è divisibile sia per 3 che per 5 e nel caso restituisco 'FizzBuzz' al posto del numero
   if (i % 3 === 0 && i % 5 === 0) {
-    console.log('FizzBuzz');
+    console.log(`FizzBuzz (${i})`);
   }
   else
+    /*se non sono divisibili per entrambi, ma per uno solo o nessuno, vado nell'else e controllo se è divisibile solo per 3 e nel caso restituisco 'Fizz' al posto del numero*/
     if (i % 3 === 0) {
-      console.log('Fizz');
+      console.log(`Fizz (${i})`);
     }
     else
+      //se non è divisibile neanche per 3 allora controllo se è divisibile per 5 e nel caso restituisco 'Buzz' al posto del numero
       if (i % 5 === 0) {
-        console.log('Buzz');
+        console.log(`Buzz (${i})`);
       }
       else
+        //se non è divisibile neanche per 5, restituisco solo il numero
         console.log(i);
 }
